ed95824377b1696e0e5e42f5e877618cd7aa201b
Gabe Bravo <gabecbravo@gmail.com>
2019-04-26 15:46:08 -0400

    Initial files for basic express test api using Typescript

---
+++ server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+const app = express();
+
+// TEST ROUTES : 
+app.get('/', (req, res, next) => {
+  res.send("Tour Booking API")
+})
+
+app.get('/tours', (req, res, next) => {
+  res.send("Get a list of tours...")
+})
+
+app.post('/tours', (req, res, next) => {
+  res.send("Add a new tour...")
+})
+
+app.listen(process.env.PORT || 8091, () => {
+  console.log('Server started...')
+})
