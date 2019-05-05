26c6c20311260799784852ea1c528518590bdba0
Gabe Bravo <gabecbravo@gmail.com>
2019-05-04 20:35:57 -0400

    Changed it from the different example version to an interface

--- api/tours/apiCreateTour.ts
+++ api/tours/apiCreateTour.ts
@@ -2,31 +2,7 @@ import { RequestHandler } from "express";
 import uuid from "uuid/v4";
 import { DataStore } from "../../data/data";
 
-// CLASS ALTERNATIVE, RATHER THAN TYPE INSTANCE
-
-// class NewTour {
-//   constructor(
-//     public id: string,
-//     public location: string,
-//     public tourTitle: string,
-//     public tourCategory: string,
-//     public tourDescription: string,
-//     public price: number,
-//     public currency: string
-//   ){}
-// }
-
-// const newTour = new NewTour(
-//   uuid(),
-//   req.body.location || "",
-//   req.body.tourTitle || "",
-//   req.body.tourCategory || "",
-//   req.body.tourDescription || "",
-//   req.body.price || 0,
-//   req.body.currency || ""
-// )
-
-type TNewTour = {
+interface INewTour {
   id: string,
   location: string,
   tourTitle: string,
@@ -38,7 +14,7 @@ type TNewTour = {
 
 export const apiCreateTour: RequestHandler = (req, res, next) => {
 
-    const testTour: TNewTour = {
+    const testTour: INewTour = {
       id: uuid(),
       location: req.body.location || "",
       tourTitle: req.body.tourTitle || "",
