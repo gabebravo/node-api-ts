4225491b25d2628897ab058ea918d4272deb8639
Gabe Bravo <gabecbravo@gmail.com>
2019-05-10 21:29:30 -0400

    Added more external middleware. Specifically Morgan for logging.

--- build/server.js
+++ build/server.js
@@ -10,25 +10,22 @@ var __importStar = (this && this.__importStar) || function (mod) {
     return result;
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const express_1 = __importDefault(require("express"));
+const app = express_1.default();
+// EXTERNAL MIDDLEWARE
+const morgan_1 = __importDefault(require("morgan"));
+const logger = morgan_1.default('dev');
+const bodyParser = __importStar(require("body-parser"));
+const jsonParser = bodyParser.json();
+// const urlEncodedParser = bodyParser.urlencoded({ extended: true })
+// MIDDLEWARE FUNCTIONS WILL RUN EVERY TIME
+app.use(logger);
+// IMPORTED ROUTES
 const apiGetTours_1 = require("./api/tours/apiGetTours");
 const apiGetTourDetail_1 = require("./api/tours/apiGetTourDetail");
 const apiCreateTour_1 = require("./api/tours/apiCreateTour");
 const apiDeleteTour_1 = require("./api/tours/apiDeleteTour");
 const apiUpdateTour_1 = require("./api/tours/apiUpdateTour");
-const express_1 = __importDefault(require("express"));
-const app = express_1.default();
-const logger = (req, res, next) => {
-    console.log(`${new Date()} - ${req.method} Request to ${req.path}`);
-    next();
-};
-const bodyParser = __importStar(require("body-parser"));
-const jsonParser = bodyParser.json();
-// INTERACTING MIDDLEWARE  - will change req/res objects
-const 
-// MIDDLEWARE FUNCTION WILL RUN EVERY TIME
-app, use;
-(logger);
-// if you wanted it just for a ceratin route, you could define that as the first arg - exmaple '/tours'
 // TEST ROUTE : 
 app.get('/', (req, res, next) => {
     res.send("Tour Booking API");
@@ -38,6 +35,7 @@ app.get("/tours", apiGetTours_1.apiGetTours);
 app.get("/tours/:id", apiGetTourDetail_1.apiGetTourDetail);
 // POST API ROUTE - jsonParser arg is passed in as middleware (will run each time first)
 app.post('/tours', jsonParser, apiCreateTour_1.apiCreateTour);
+// app.post('/tours', urlEncodedParser, apiCreateTour) - FOR URL ENCODING
 // DELETE API ROUTE
 app.delete("/tours/:id", apiDeleteTour_1.apiDeleteTour);
 // PATCH API ROUTE
--- server.ts
+++ server.ts
@@ -1,29 +1,15 @@
 import express from 'express';
 const app = express();
 
+// EXTERNAL MIDDLEWARE
+import morgan from 'morgan'
+const logger = morgan('dev')
 import * as bodyParser from 'body-parser';
-import { RequestHandler } from "express-serve-static-core";
-import { runInNewContext } from "vm";
-import { CustomRequestHandler } from "./model/express";
 const jsonParser = bodyParser.json()
-
-// INTERACTING MIDDLEWARE  - will change req/res objects
-const authenticator: CustomRequestHandler = (req, res, next) => {
-  const username = 'Andy123';
-  req.user = username;
-  next();
-}
-
-// PASSIVE MIDDLEWARE
-const logger: CustomRequestHandler = (req, res, next) => {
-  console.log(`User: ${req.user} ${new Date()} - ${req.method} Request to ${req.path}`)
-  next();
-}
+// const urlEncodedParser = bodyParser.urlencoded({ extended: true })
 
 // MIDDLEWARE FUNCTIONS WILL RUN EVERY TIME
-app.use(authenticator)
 app.use(logger) 
-// if you wanted it just for a ceratin route, you could define that as the first arg - exmaple '/tours'
 
 // IMPORTED ROUTES
 import { apiGetTours } from "./api/tours/apiGetTours";
@@ -43,6 +29,7 @@ app.get("/tours/:id", apiGetTourDetail);
 
 // POST API ROUTE - jsonParser arg is passed in as middleware (will run each time first)
 app.post('/tours', jsonParser, apiCreateTour)
+// app.post('/tours', urlEncodedParser, apiCreateTour) - FOR URL ENCODING
 
 // DELETE API ROUTE
 app.delete("/tours/:id", apiDeleteTour);
